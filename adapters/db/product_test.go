09d61c0ec9366a7d32b6424bac59cced200653b6
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-10-29 20:31:01 -0300

    feat: implement database setup for tests

---
+++ adapters/db/product_test.go
@@ -0,0 +1,39 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+)
+
+var Db *sql.DB
+
+func setUp() {
+	Db, _ = sql.Open("sqlite3", ":memory:")
+	createTable(Db)
+	createProduct(Db)
+}
+
+func createTable(db *sql.DB) {
+	table := `
+		CREATE TABLE products (	
+			"id" string,
+			"name" string,
+			"price" float,
+			"status" string
+		);`
+
+	stmt, err := db.Prepare(table)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	stmt.Exec()
+}
+
+func createProduct(db *sql.DB) {
+	insert := `INSERT INTO products (id, name, price, status) VALUES ("abc", "Product Test", 0, "disabled");`
+	stmt, err := db.Prepare(insert)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	stmt.Exec()
+}

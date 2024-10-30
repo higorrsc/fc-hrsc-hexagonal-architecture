7fbe03f80ecd71bd7f0536b2eca8a2caa1fa9917
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-10-30 20:58:57 -0300

    feat: implement CLI with Cobra

--- main.go
+++ main.go
@@ -1,21 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"database/sql"
-
-	db2 "github.com/higorrsc/fc-hrsc-hexagonal-architecture/adapters/db"
-	"github.com/higorrsc/fc-hrsc-hexagonal-architecture/application"
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/higorrsc/fc-hrsc-hexagonal-architecture/cmd"
 
 func main() {
-	db, _ := sql.Open("sqlite3", "db.sqlite3")
-	defer db.Close()
-
-	productDbAdapter := db2.NewProductDb(db)
-	productService := application.NewProductService(productDbAdapter)
-
-	product, _ := productService.Create("Product Example", 30)
-	productService.Enable(product)
-
+	cmd.Execute()
 }
